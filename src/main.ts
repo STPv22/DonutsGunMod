4acec37346ba216bdbcbb03db109b91913836783
Stpv22 <145866689+STPv22@users.noreply.github.com>
2024-12-04 18:29:24 +0000

    SOOON

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,21 @@
-ModAPI.displayToChat({msg: "hello, this is eaglerforge!"})
-ModAPI.logger.loginfo({string: "you can log a string to console"})
-ModAPI.uwuify({string: "and even uwuify a string?"})
+import { addRecipe } from "./addRecipe";
+
+//ModAPI.meta.icon("");
+ModAPI.meta.title("Donut's Bullshit Crafting");
+ModAPI.meta.version("v0.1");
+ModAPI.meta.description("Based off of this video: <a>https://www.youtube.com/watch?v=syPUpzWGlJY</a>");
+ModAPI.meta.credits("STPv22");
+
+addRecipe("diamond_block", 
+    true, 
+    {
+        "D": {
+            type: "block",
+            id: "dirt" // Using dirt blocks
+        }
+    },
+    [
+        "DDD",
+        "DDD",
+        "DDD"
+    ]);
