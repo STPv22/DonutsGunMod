bfe35c5ea88f68ade4bb25c9ce914cbdc612aa5f
Stpv22 <145866689+STPv22@users.noreply.github.com>
2024-12-16 20:58:55 +0000

    more

--- src/main.ts
+++ src/main.ts
@@ -19,3 +19,54 @@ addRecipe("diamond_block",
         "DDD",
         "DDD"
     ]);
+
+    addRecipe("iron_shovel", 
+        false, 
+        {
+            "D": {
+                type: "item",
+                id: "stick"
+            },
+            "#": {
+                type: "block",
+                id: "tnt"
+            }
+        },
+        [
+            " # ",
+            " D ",
+            " D "
+        ]);
+        addRecipe("gold_pickaxe", 
+            false, 
+            {
+                "D": {
+                    type: "block",
+                    id: "gold" // Using dirt blocks
+                },
+                "#": {
+                    type: "item",
+                    id: "stick" // Using dirt blocks
+                },
+                "S": {
+                    type: "block",
+                    id: "soulsand" // Using dirt blocks
+                },
+                "O": {
+                    type: "item",
+                    id: "firecharge" // Using dirt blocks
+                },
+                "V": {
+                    type: "item",
+                    id: "magma_cream" // Using dirt blocks
+                },
+                "G": {
+                    type: "item",
+                    id: "leather" // Using dirt blocks
+                }
+            },
+            [
+                "DDD",
+                "S#O",
+                "V#G"
+            ]);
